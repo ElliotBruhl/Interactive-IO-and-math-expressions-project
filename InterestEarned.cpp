d58bddde97610f0e6fad415ac31872359dc3d27d
ElliotBruhl <bruhl23188@outlook.com>
2024-09-13 10:16:07 -0400

    in progress

---
+++ InterestEarned.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using std::cout, std::cin;
+
+int main() {
+    //total = Principal * (1 + rate/t)^t
+    double rate;
+    int timesCompounded;
+    double principal;
+
+    cout << "What is the interest rate? ";
+    cin >> rate;
+    cout << "How many times is interest compounded? ";
+    cin >> timesCompounded;
+    cout << "What is the principal (initial amount)? ";
+    cin >> principal;
+
+    cout << ""
+
+    return 0;
+}
